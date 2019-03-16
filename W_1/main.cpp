f9b1a4cb0e8569ef40decdf432490e2a5e67246a
Артём <artem@artems-macbook-pro.local>
2019-03-16 19:11:22 +0300

    Initial Commit

---
+++ W_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  W_1
+//
+//  Created by Артём on 16/03/2019.
+//  Copyright © 2019 Ерошев Артём. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
